067ba7c49f9fc78f22b9ca8424e3d5252ab61b39
Rajib <rajib@iglulabs.com>
2022-07-24 19:15:05 +0530

    1.created superuser by using command like python manage.py createsuperuser 2.install pillow to use images through media by using command python -m pip install Pillow 3.added first model named slider in model.py file 4.run : python manage.py makemigrations 5.run : python manage.py migrate 6.run : python manage.py runserver

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+SLIDER_STATUS_CHOICES = (
+       ('Active','Active'),
+       ('Inactive', 'Inactive'),
+    )
+class slider(models.Model):
+    title = models.CharField(max_length=500)
+    image = models.ImageField(upload_to="home/slider")
+    status = models.CharField(max_length=500, choices=SLIDER_STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title + ' added new slider '
