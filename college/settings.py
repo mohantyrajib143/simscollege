e3e4a58a2c3b7e6e662368624aaee95bab74791c
Rajib <rajib@iglulabs.com>
2022-07-24 12:17:26 +0530

    1.created static folder and add all static files like css, js, images 2.modified urls.py for static files 3.modified settings.py for static files

--- college/settings.py
+++ college/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+from pathlib import Path, os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR,'static')
+]
