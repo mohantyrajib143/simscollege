fbf0523268b8e6d6b34c1e59daa784b7638a19e8
Rajib <rajib@iglulabs.com>
2022-07-24 19:48:46 +0530

    fetch all slider data to index file

--- website/views.py
+++ website/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from website.models import slider
 
 # Create your views here.
 def index(request):
-    return render(request,'website/index.html')
+    allSlider = slider.objects.filter(status='Active')
+    data = {'allSlider':allSlider}
+    return render(request,'website/index.html', data)
