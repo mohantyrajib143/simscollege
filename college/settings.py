90f90574ef6da2306d332a226221a76b6a0ee976
Rajib <rajib@iglulabs.com>
2022-07-24 11:01:49 +0530

    1.Created database named collge manually 2.modified settings.py for mysql database 3.sudo apt-get install python3-dev default-libmysqlclient-dev build-essential 4.sudo apt install python3-pip 5.pip install mysqlclient 6.python manage.py migrate 7.python manage,py runserver

--- college/settings.py
+++ college/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'college.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'college',
+        'HOST': 'localhost',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'PORT': '3306',
     }
 }
 
