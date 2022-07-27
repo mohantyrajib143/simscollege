b2ff93f65d5730f10f9740312352b993519efc68
Rajib <rajib@iglulabs.com>
2022-07-27 22:19:04 +0530

    added sims model in views.py for showing records in index.html file

--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from website.models import slider
+from website.models import slider, sims
 
 # Create your views here.
 def index(request):
     allSlider = slider.objects.filter(status='Active')
-    data = {'allSlider':allSlider}
+    simsInfo = sims.objects.filter(id=1)
+    data = {'allSlider':allSlider, 'simsInfo':simsInfo[0]}
     return render(request,'website/index.html', data)
