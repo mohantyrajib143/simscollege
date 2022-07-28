197d12d50720d1e2aae9eea2b9832eb55e852767
Rajib <rajib@iglulabs.com>
2022-07-28 22:31:26 +0530

    1.imported student_testmonials model in views.py, and fetched all data 2.added forloop to student_testmonials in index.html

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from website.models import slider, sims, summer_course_enquiry
+from website.models import slider, sims, summer_course_enquiry, student_testmonials
 
 # Create your views here.
 def index(request):
@@ -20,6 +20,7 @@ def index(request):
         return JsonResponse(msg)
     else:
         allSlider = slider.objects.filter(status='Active')
+        stdTestmonial = student_testmonials.objects.all().order_by('-id')
         simsInfo = sims.objects.filter(id=1)
-        data = {'allSlider':allSlider, 'simsInfo':simsInfo[0]}
+        data = {'allSlider':allSlider, 'simsInfo':simsInfo[0], 'stdTestmonial':stdTestmonial}
         return render(request,'website/index.html', data)
