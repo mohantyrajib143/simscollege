9660dd39dc58cd9009c793852794a83f511cf139
Rajib <rajib@iglulabs.com>
2022-07-28 22:04:48 +0530

    1.Import JsonResponse, summer_course_enquiry 2.modified index() in views.py for storing summer_course_enquiry for ajax submission 3.added ValidateName for validate name to allow only text 4.added ajax to submit form without refresh 5.added input field validation for mobile no

--- website/views.py
+++ website/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from website.models import slider, sims
+from django.http import HttpResponse, JsonResponse
+from website.models import slider, sims, summer_course_enquiry
 
 # Create your views here.
 def index(request):
-    allSlider = slider.objects.filter(status='Active')
-    simsInfo = sims.objects.filter(id=1)
-    data = {'allSlider':allSlider, 'simsInfo':simsInfo[0]}
-    return render(request,'website/index.html', data)
+    msg={}
+    if request.method=='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        mobile = request.POST['mobile']
+        stream = request.POST['stream']
+        message = request.POST['message']
+
+        data = summer_course_enquiry(name=name, email=email, mobile=mobile, stream=stream, message=message)
+        data.save()
+
+        if data.id != 0:
+            msg = { "status": 1 } 
+        return JsonResponse(msg)
+    else:
+        allSlider = slider.objects.filter(status='Active')
+        simsInfo = sims.objects.filter(id=1)
+        data = {'allSlider':allSlider, 'simsInfo':simsInfo[0]}
+        return render(request,'website/index.html', data)
