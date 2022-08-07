6c1e896d02c69cf1154accd311814adc9e70f962
Rajib <rajib@iglulabs.com>
2022-08-07 17:35:47 +0530

    Modified header.html and added about in context processor, modified careers_info.html

--- website/context_processor.py
+++ website/context_processor.py
@@ -1,5 +1,6 @@
-from website.models import sims
+from website.models import sims, about
 def context_processor(request):
     data = {}
     data['sims'] = sims.objects.filter(id=1)[:1]
+    data['about'] = about.objects.filter(id=1)[:1]
     return data
