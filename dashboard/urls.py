6ec83ebcb09a489cea3e0c7d4cecb1e1456c03c8
Rajib <rajib@iglulabs.com>
2022-08-09 21:03:35 +0530

    added forgot_pass.html file in templates with urls and views

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from dashboard import views
 
 urlpatterns = [
-    path('', views.login, name='login')
+    path('', views.login, name='login'),
+    path('forgot_pass/', views.forgot_pass, name='forgot_pass'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def login(request):
     return render(request, 'dashboard/login.html')
+
+def forgot_pass(request):
+    return render(request, 'dashboard/forgot_pass.html')
